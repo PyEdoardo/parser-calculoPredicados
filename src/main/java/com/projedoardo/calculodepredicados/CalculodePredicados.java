2263099abad7dbde4e1e4d53d533ab1e42af1a04
edoardo <edoardotombolesi8@gmail.com>
2024-08-19 23:55:30 -0300

    adição arquivos do projeto

---
+++ src/main/java/com/projedoardo/calculodepredicados/CalculodePredicados.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.projedoardo.calculodepredicados;
+
+/**
+ *
+ * @author edoar
+ */
+public class CalculodePredicados {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
